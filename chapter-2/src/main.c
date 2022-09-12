a235b1f8092f4830250cb26a97d7f2d6c22675cf
KellSkog <kell.skog@gmail.com>
2022-09-12 09:23:20 +0200

    Complete chapter 2.5

--- chapter-2/src/main.c
+++ chapter-2/src/main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+#define ALLOWED_CU_INCH_PER_POUND 166
+
+int dimensionalWeight(int volume)
+{
+    // Assure division is rounded up
+    return (volume + (ALLOWED_CU_INCH_PER_POUND - 1)) / ALLOWED_CU_INCH_PER_POUND;
+}
+
+int main(int argc, char **argv)
 {
     printf("To C, or not C: that is the question.\n");
-
+    int height, length, width;
+    int volume;
+    if (argc == 4) // Three arguments
+    {
+        // TODO: Prefere stol
+        height = atoi(argv[1]);
+        length = atoi(argv[2]);
+        width = atoi(argv[3]);
+    }
+    else
+    {
+        printf("Enter height: ");
+        scanf("%d", &height);
+        printf("Enter length: ");
+        scanf("%d", &length);
+        printf("Enter width: ");
+        scanf("%d", &width);
+    }
+    volume = (height * length * width);
+    printf("Dimensional weight: %d\n", dimensionalWeight(volume));
     return 0;
 }
