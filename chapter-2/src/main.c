7f2234e715a9ea4688f5374f5ebc4f3a58836c1a
KellSkog <kell.skog@gmail.com>
2022-09-12 09:52:12 +0200

    Complete chap 2.6

--- chapter-2/src/main.c
+++ chapter-2/src/main.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ALLOWED_CU_INCH_PER_POUND 166
+#include "constants.h"
 
 int dimensionalWeight(int volume)
 {
     // Assure division is rounded up
     return (volume + (ALLOWED_CU_INCH_PER_POUND - 1)) / ALLOWED_CU_INCH_PER_POUND;
 }
-
-int main(int argc, char **argv)
+void calcWeight(int argc, char **argv)
 {
-    printf("To C, or not C: that is the question.\n");
     int height, length, width;
     int volume;
     if (argc == 4) // Three arguments
@@ -32,5 +30,33 @@ int main(int argc, char **argv)
     }
     volume = (height * length * width);
     printf("Dimensional weight: %d\n", dimensionalWeight(volume));
+}
+
+float farenheitToCelisius(float farenheit_temp)
+{
+    return (farenheit_temp - FARENHEIT_FREEZING_PT) * FARENHEIT_CELSIUS_SCALE;
+}
+void calcTemp(int argc, char **argv)
+{
+    float farenheit;
+    if (argc == 2) // Three arguments
+    {
+        farenheit = atof(argv[1]);
+    }
+    else
+    {
+        printf("Farenheit temperature: ");
+        scanf("%f", &farenheit);
+    }
+    printf("Farenheit %.1f -> Celsius: %.1f\n", farenheit, farenheitToCelisius(farenheit));
+}
+
+int main(int argc, char **argv)
+{
+    printf("To C, or not C: that is the question.\n");
+    /*calcWeight(argc, argv);
+     */
+    calcTemp(argc, argv);
+
     return 0;
 }
