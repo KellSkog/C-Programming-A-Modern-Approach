aff71a1f5ede6a1d7e1f876e255f7d91802e06fb
KellSkog <kell.skog@gmail.com>
2022-09-13 09:10:59 +0200

    Add program 5.3 Printing a date in Legal form

--- chapter-2/src/main.c
+++ chapter-2/src/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define __USE_MISC // defines M_PI...
+#include <math.h>
+#include <string.h>
 
 #include "constants.h"
 
@@ -51,12 +54,93 @@ void calcTemp(int argc, char **argv)
     printf("Farenheit %.1f -> Celsius: %.1f\n", farenheit, farenheitToCelisius(farenheit));
 }
 
+void printPrelude(int row)
+{
+    if (row == 3)
+        printf("* ");
+    else if (row == 4)
+        printf(" *");
+    else
+        printf("  ");
+}
+void printEnding(int row)
+{
+    for (int j = 0; j < (5 - row); ++j)
+    {
+        printf(" ");
+    }
+    printf("*\n");
+}
+
+void programmingProjects_2_1(void)
+{
+    const int rows = 6;
+    // const int cols = 8;
+    for (int row = 0; row < rows; ++row)
+    {
+        printPrelude(row);
+        printEnding(row);
+    }
+}
+
+float calcSphereVol(float radius)
+{
+    return radius * radius * radius * 4.0f * M_PI / 3.0f;
+}
+void programmingProjects_2_2(void)
+{
+    printf("Enter a radius: ");
+    float radius;
+    scanf("%f", &radius);
+    printf("A spere with radius: %.3f, has the volume %.3f\n", radius, calcSphereVol(radius));
+}
+
+void printingDate_5_3(void)
+{
+    const char *months[] = {"", // No month 0
+                            "January",
+                            "February",
+                            "Mars",
+                            "April",
+                            "May",
+                            "June",
+                            "July",
+                            "August",
+                            "September",
+                            "October",
+                            "November",
+                            "December"};
+    printf("Enter date (mm/dd/yy): ");
+    int day, month, year;
+    scanf("%d/%d/%d", &month, &day, &year);
+    printf("Dated this %d", day);
+    switch (day)
+    { // "st", "rd", "th"
+    case 21:
+    case 31:
+        printf("st ");
+        break;
+    case 23:
+        printf("rd ");
+        break;
+    default:
+        printf("th ");
+    }
+    printf("day of %s. ", months[month]);
+    printf("20%d.\n", year);
+}
+
 int main(int argc, char **argv)
 {
-    printf("To C, or not C: that is the question.\n");
+    printf("To %f\n, or not C: that is the question.\n", M_PI);
     /*calcWeight(argc, argv);
-     */
     calcTemp(argc, argv);
+    programmingProjects_2_1();
+    programmingProjects_2_2();
+     */
+    (void)argc;
+    (void)argv;
+    printingDate_5_3();
 
     return 0;
 }
